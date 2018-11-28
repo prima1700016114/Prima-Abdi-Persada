bec2a03bb51740e2c3d272cb89282d7cc56903ba
Prima-Abdi-Persada <prima1700016114@webmail.uad.ac.id>
2018-11-28 13:13:30 +0700

    versi 2.3.2

---
+++ src/pages/setting/setting.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-setting',
+  templateUrl: 'setting.html'
+})
+export class settingPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
